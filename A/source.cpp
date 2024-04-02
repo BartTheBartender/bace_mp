4fbd29f4ab2df625083886b1718a42a609a92d5f
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-04-02 13:02:04 +0200

    geomseq done

--- A/source.cpp
+++ A/source.cpp
@@ -6,8 +6,6 @@
 #include <cmath>
 using namespace std;
 
-void d(){cout << "TU" << endl;}
-
 bool equal(float p, float q){
   return fabs((p-q)/q) < 1e-6 || fabs((p-q)) < 1e-25;
 }
@@ -15,16 +13,6 @@ bool equal(float p, float q){
 float cube_root(float a) {
     assert(a != 0);
     float x = ((a > 0) - (a < 0)) * pow(fabs(a),1.0/3.0);
-    /* float precision = 10e-4; */
-    float y;
-    size_t i = 0;
-
-    do {
-        /* cout << i++ << " " << x << endl; */
-        y = x;
-        x = (2 * x + a / (x * x)) / 3.0; 
-    } while (!equal((y-x),0.f) && i < 1e6);
-
     return x;
 }
 
@@ -32,7 +20,6 @@ int main () {
 
   cout.setf(std::ios_base::scientific, std::ios_base::floatfield);
   cout << std::setprecision(10);
-  /* cout << std::setprecision(2); */
 
   size_t n;
   cin >> n;
@@ -40,8 +27,6 @@ int main () {
   for (size_t i = 0; i < n; ++i) {
     float p, s;
     cin >> p >> s;
-    /* cout << "new set: p: " << p << " s: " << s << endl; */
-
     if (p == 0) {
       cout << 0.f << " " << 0.f << " " << 0.f << endl;
     } else {
@@ -54,8 +39,6 @@ int main () {
       if (q < 2 && q > -2) {
         cout << 0.f << " " << 0.f << " " << 0.f << endl;
       } else {
-        /* float u = s - b; */
-        /* float v = p/b; */
         float r = (q + sqrt(q*q-4))/2;
 
         assert(!isinf(r) && !isnan(r));
@@ -66,10 +49,8 @@ int main () {
 
         assert(!isinf(r) && !isnan(r));
         assert(r != 0);
-
-        /* cout << "r: " << r << endl; */
-        
         float a = b*r, c = b/r;
+
         //let a be the greater one;
         if (a < c) {
           float temp = a;
@@ -80,31 +61,11 @@ int main () {
         assert(a >= c);
         assert(equal(a+b+c,s));
         assert(equal(a*b*c,p));
-
         cout << a << " " << b << " " << c << endl;
-
-
-
-
-
-
       }
-
-
-
     }
     
   }
 
 }
-
-/* int main() { */
-/*   cout.setf(std::ios_base::scientific, std::ios_base::floatfield); */
-/*   cout << std::setprecision(10); */
-/*   for(size_t i = 0; i < 9; ++i){ */
-/*     float x; */
-/*     cin >> x; */
-/*     cout << cube_root(x) << endl; */
-/*   } */
-/* } */
 
