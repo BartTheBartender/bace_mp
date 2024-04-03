5fa2381cd2611bb676ccf8e9513591deda917728
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-04-03 20:36:44 +0200

    1/3 newtona pierdolona

--- C/main.cpp
+++ C/main.cpp
@@ -1,34 +1,24 @@
 #include "source.cpp"
 
-  void implicitCurve(const double* x, double* y, double* Df){
-    // funkcja dana jest wzorem f(a,b,c) = (1-a^2-b^2-c^2,(a+b+c)/(a^2+b^2+c^2)-1)
+  void implicitSurface(const double* x, double* y, double* Df){
+    // funkcja dana jest wzorem f(a,b,c) = (a+b+c)/(a^2+b^2+c^2)-1
     // zmienne pomocnicze
     const double n = x[0]*x[0] + x[1]*x[1] + x[2]*x[2];
-    const double r = 1./n;
-    const double s = (x[0] + x[1] + x[2])*r;
+    const double s = x[0] + x[1] + x[2];
     
     // obliczam wartosc funkcji
-    y[0] = 1. - n;
-    y[1] = s - 1.;
+    *y = s/n - 1.;
     
-    //obliczam pierwszy wiersz macierzy
-    Df[0] = -2.*x[0];
-    Df[1] = -2.*x[1];
-    Df[2] = -2.*x[2];
-    
-    //obliczam drugi wiersz macierzy
-    const double r2 = 2.*s*r;
-    Df[3] = r - x[0]*r2;
-    Df[4] = r - x[1]*r2;
-    Df[5] = r - x[2]*r2;
+    //obliczam pierwszy i jedyny wiersz macierzy
+    const double r = 1./n;
+    const double r2 = 2.*(*y)*r;
+    Df[0] = r - x[0]*r2;
+    Df[1] = r - x[1]*r2;
+    Df[2] = r - x[2]*r2;
   }
-  
+
   int main(){
     double x[3] = {0.25*(1.+sqrt(5.)),0.25*(1.-sqrt(5.)),0.5};
-    findCurve(implicitCurve,x,10,1./128);
-    printf("\n");
-    double x2[3] = {0.25*(1.-sqrt(5.)),0.25*(1.+sqrt(5.)),0.5};
-    int i = findCurve(implicitCurve,x2,10,3./32);
-    printf("%d",i);
+    findSurface(implicitSurface,x,4,4,1./32,1./32);
     return 0;
   }
--- C/source.cpp
+++ C/source.cpp
@@ -1,60 +1,107 @@
 //Bartosz Furmanek
-#include <iostream>
-#include <iomanip>
+#include <cstdio>
 #include <cmath>
 #include <cassert>
 using namespace std;
 
+void print_vector(const double* x, unsigned N){
+  for(unsigned i=0;i<N;++i)
+    printf("%17.17f ",x[i]);
+  printf("\n");
+}
+
+
 typedef void (*FuncPointer)(const double* x, double* y, double* Df);
 
+bool is_correct(double* y, size_t n) {
+  for(size_t i = 0; i < n; ++i)
+    if(isnan(y[i]) || isinf(y[i]))
+        return false;
+
+  return true;
+}
+
 int findCurve(FuncPointer f, const double* x, unsigned k, double h) {  
-  cout << std::setprecision(17);
+
 
   double y[2];
   double Df[2*3];
   double u[3];
-  double d00, d01, d10, d11;
+  u[2] = x[2];
+  double d00, d01, d10, d11, det;
 
 
   for(unsigned i = 1; i <= k; ++i) {
-    u[0] = x[0] + i*h;
-    u[1] = x[1] + i*h;
-    u[2] = x[2] + i*h;
 
+    u[0] = x[0];
+    u[1] = x[1];
+    u[2] += h;
     
-    for(unsigned _ = 0; _ < 100000; ++_) {
+    for(unsigned _ = 0; _ < 1000; ++_) {
+
       f(u,y,Df);
+
+
+      if(fabs(y[0]) < 1e-14 && fabs(y[1]) < 1e-14)
+        break;
 
       d00 = Df[0];
       d01 = Df[1];
       d10 = Df[3];
       d11 = Df[4];
+      det = d00 * d11 - d01 * d10;
 
-      if(d00 * d11 - d01 * d10 == 0){
+      if(det == 0 || !is_correct(y,2))
         return i;
-      }
 
-      u[0] -= (d11 * y[0] - d01 * y[1])/(d11 * d00 - d10 * d01);
-      u[1] -= (-d10 * y[0] + d00 * y[1])/(d11 * d00 - d10 * d01);
+      u[0] -=  (d11 * y[0] - d01 * y[1]) / det;
+      u[1] -= (-d10 * y[0] + d00 * y[1]) / det;
+      
 
-      if(fabs(y[0]) < 10e-14 && fabs(y[1]) < 10e-14)
-        break;
     }
 
-      if(fabs(y[0]) < 10e-14 && fabs(y[1]) < 10e-14){
-        cout << u[0] << " " << u[1] << " " << u[2] << endl;
-      } else {
+      if(fabs(y[0]) < 1e-14 && fabs(y[1]) < 1e-14)
+        print_vector(u,3);
+      else
         return i;
-      }
   }
 
   return 0;
 }
 
-int findSurface(FuncPointer f, double* x, unsigned k1, unsigned k2, double h1, double h2) {
-  assert(false);
+int findSurface(FuncPointer f,
+    double* x, unsigned k1, unsigned k2, double h1, double h2) {
+  
+  double y[1];
+  double Df[1*3];
+  double u[3];
+  double df;
+  
+  u[1] = x[1];
+  u[2] = x[2];
+
+  for(unsigned i1 = 0; i1 < k1; ++i1) {
+
+    u[1] += h1;
+    u[2] = x[2];
+
+    for(unsigned i2 = 0; i2 < k2; ++i2) {
+
+      u[2] += h2;
+      u[0] = x[0];
+
+      for(unsigned _ = 0; _ < 10000; ++_) {
+          
+        f(u,y,Df);
+        df = Df[0];
+
+        if(!is_correct(y,1) || df != 0);
+      }
+    }
+  }
+  
 }
 int findFixedPoints(FuncPointer f, double* x, unsigned k1, unsigned k2, double h1, double h2) {
-  assert(false);
+  
 }
 
